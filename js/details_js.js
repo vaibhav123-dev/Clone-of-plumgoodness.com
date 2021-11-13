bfd4536fb380ab524bb6bde41d404b34464106f8
DILIP KUMAR JAKHAR <jakhardilip10@gmail.com>
2021-11-13 19:33:12 +0530

    my cart page created

---
+++ js/details_js.js
@@ -0,0 +1,268 @@
+// var data = [
+//     {
+//       Img_url:
+//         "https://cdn.shopify.com/s/files/1/0390/2985/products/10.GTFacewash-1_compact.jpg?v=1630925464",
+//       Name: "Green Tea Pore Cleansing Face Wash | Clears Acne, Unclogs Pores & Controls Oil | With Glycolic Acid & Green Tea Extracts | Non-drying, Soap-Free, 100% Vegan",
+//       pricecut: 3999,
+//       Price: 310,
+//       Review: 1167 + " reviews",
+//     },
+//     {
+//       Img_url:
+//         "https://cdn.shopify.com/s/files/1/0390/2985/products/3.HAMoisturizer-1_150x.jpg?v=1630925406",
+//       Name: "Hello Aloe Caring Day Moisturizer | For Normal, Dry, Very Dry, Sensitive Skin | SLS Free, 100% Vegan",
+//       pricecut: 3999,
+//       Price: 423,
+//       Review: 157 + " reviews",
+//     },
+//     {
+//       Img_url:
+//         "https://cdn.shopify.com/s/files/1/0390/2985/products/4.HASPF-1_150x.jpg?v=1630926060",
+//       Name: "Hello Aloe Ultra Lite Day Lotion SPF 20",
+//       pricecut: 3999,
+//       Price: 427,
+//       Review: 99 + " reviews",
+//     },
+//     {
+//       Img_url:
+//         "https://cdn.shopify.com/s/files/1/0390/2985/products/9.GTToner-1_150x.jpg?v=1630924008",
+//       Name: "Green Tea Alcohol-Free Toner  | Shrinks Pores & Gives Even-Tone Skin | 100% Vegan",
+//       pricecut: 3999,
+//       Price: 351,
+//       Review: 1285 + " reviews",
+//     },
+//     {
+//       Img_url:
+//         "https://cdn.shopify.com/s/files/1/0390/2985/products/CandyMeltsVeganLipBalm_BerryFeast_01_150x.jpg?v=1630923446",
+//       Name: "Candy Melts Vegan Lip Balm | Berry Feast | With Natural UV Protection | 100% Cruelty Free",
+//       pricecut: 3999,
+//       Price: 265,
+//       Review: 227 + " reviews",
+//     },
+//     {
+//       Img_url:
+//         "https://cdn.shopify.com/s/files/1/0390/2985/products/8.GTMM-1_150x.jpg?v=1630925114",
+//       Name: "Green Tea Mattifying Moisturizer | Oily, Acne-Prone Skin | SLS-Free, 100% Vegan",
+//       pricecut: 3999,
+//       Price: 423,
+//       Review: 789 + " reviews",
+//     },
+//     {
+//       Img_url:
+//         "https://cdn.shopify.com/s/files/1/0390/2985/products/ListingImages_5a_150x.jpg?v=1630923420",
+//       Name: "Candy Melts Lip Loves - Pack of 2 Lip Balms | Red Velvet Love + Berry Feast",
+//       pricecut: 3999,
+//       Price: 499,
+//       Review: 207 + " reviews",
+//     },
+//     {
+//       Img_url:
+//         "https://cdn.shopify.com/s/files/1/0390/2985/products/ListingImages_6a_150x.jpg?v=1630923407",
+//       Name: "Green Tea Clear Face Mask  | Clay Mask For Oily Skin | 100% Vegan",
+//       pricecut: 3999,
+//       Price: 441,
+//       Review: 881 + " reviews",
+//     },
+//     {
+//       Img_url:
+//         "https://cdn.shopify.com/s/files/1/0390/2985/products/3.GSSBScrub-1_150x.jpg?v=1630923703",
+//       Name: "Candy Melts Lip Loves - Pack of 2 Lip Balms | Mint-O-Coco + Red Velvet Love",
+//       pricecut: 3999,
+//       Price: 449,
+//       Review: 97 + " reviews",
+//     },
+//     {
+//       Img_url:
+//         "https://cdn.shopify.com/s/files/1/0390/2985/products/CandyMeltsVeganLipBalm_RedVelvetLove_01_150x.jpg?v=1632452212",
+//       Name: "Grape Seed & Sea Buckthorn Mild Foam Scrub | Gentle Exfoliation | SLS Free, 100% Vegan",
+//       pricecut: 3999,
+//       Price: 305,
+//       Review: 225 + " reviews",
+//     },
+//     {
+//       Img_url:
+//         "https://cdn.shopify.com/s/files/1/0390/2985/products/1.CWTSunCare-1_150x.jpg?v=1630927468",
+//       Name: "Candy Melts Vegan Lip Balm | Red Velvet Love | With Natural UV Protection for Lips | 100% Cruelty Free",
+//       pricecut: 3999,
+//       Price: 269,
+//       Review: 353 + " reviews",
+//     },
+//     {
+//       Img_url:
+//         "https://cdn.shopify.com/s/files/1/0390/2985/products/4.GSSBNightCream-1_150x.jpg?v=1630923666",
+//       Name: "Grape Seed & Sea Buckthorn Light Stay Day Cream SPF 30 PA+++ | Sun-protection | SLS Free, 100% Vegan",
+//       pricecut: 3999,
+//       Price: 549,
+//       Review: 568 + " reviews",
+//     },
+//     {
+//       Img_url:
+//         "https://cdn.shopify.com/s/files/1/0390/2985/products/6.GSSBFaceMask-1_150x.jpg?v=1630923727",
+//       Name: "Grape Seed & Sea Buckthorn Nurturance Night Cream | For Dry, Very Dry Skin | SLS Free, 100% Vegan",
+//       pricecut: 3999,
+//       Price: 298,
+//       Review: 451 + " reviews",
+//     },
+//     {
+//       Img_url:
+//         "https://cdn.shopify.com/s/files/1/0390/2985/products/1.CWTWash-1_150x.jpg?v=1630920307",
+//       Name: "Grape Seed & Sea Buckthorn Renaissance Face Mask | Deep Exfoliation | For Dry, Very Dry Skin",
+//       pricecut: 3999,
+//       Price: 788,
+//       Review: 235 + " reviews",
+//     },
+//     {
+//       Img_url:
+//         "https://cdn.shopify.com/s/files/1/0390/2985/products/4.CWTToner-1_150x.jpg?v=1630923608",
+//       Name: "Chamomile & White Tea Revival Face Wash | Mild Cleanser | For Normal, Dry, Combination Skin",
+//       pricecut: 3999,
+//       Price: 333,
+//       Review: 121 + " reviews",
+//     },
+//     {
+//       Img_url:
+//         "https://cdn.shopify.com/s/files/1/0390/2985/products/2.CWTSPF50-1_150x.jpg?v=1630920337",
+//       Name: "Chamomile & White Tea Calming Antioxidant Toner | Vitamin B3 | For Normal, Dry, Combination Skin",
+//       pricecut: 3999,
+//       Price: 635,
+//       Review: 254 + " reviews",
+//     },
+//     {
+//       Img_url:
+//         "https://cdn.shopify.com/s/files/1/0390/2985/products/2.ELumCream-1_150x.jpg?v=1630922312",
+//       Name: "E-Luminence Deep Moisturizing Creme | Enriched with Niacinamide, Vitamin E & Willow Bark | 100% Vegan, Cruelty Free",
+//       pricecut: 3999,
+//       Price: 689,
+//       Review: 254 + " reviews",
+//     },
+//     {
+//       Img_url:
+//         "https://cdn.shopify.com/s/files/1/0390/2985/products/1WBG_12368a12-d0c2-44b7-b20a-e606d32bdbde_150x.jpg?v=1632897274",
+//       Name: "Chamomile & White Tea Brighten-Up Face Scrub | Gentle Exfoliation | For Normal, Dry, Combination Skin",
+//       pricecut: 3999,
+//       Price: 310,
+//       Review: 302 + " reviews",
+//     },
+//     {
+//       Img_url:
+//         "https://cdn.shopify.com/s/files/1/0390/2985/products/5.CWTMask-1_150x.jpg?v=1630920095",
+//       Name: "Green Tea Renewed Clarity Night Gel  | With Licorice, Willow Bark & Glycolic Acid | Oily, Acne-Prone Skin | 100% Vegan",
+//       pricecut: 3999,
+//       Price: 420,
+//       Review: 401 + " reviews",
+//     },
+//     {
+//       Img_url:
+//         "https://cdn.shopify.com/s/files/1/0390/2985/products/2.GTNightGel-1_150x.jpg?v=1630925536",
+//       Name: "Chamomile & White Tea Glow-Getter Face Mask | Clay Mask | For Normal, Dry, Combination Skin",
+//       pricecut: 3999,
+//       Price: 423,
+//       Review: 213 + " reviews",
+//     },
+//     {
+//       Img_url:
+//         "https://cdn.shopify.com/s/files/1/0390/2985/products/3.ELumSerum-1_150x.jpg?v=1630922233",
+//       Name: "Grape Seed & Sea Buckthorn Glow-Restore Face Oils Blend | Best Face Oil for Glowing Skin | Blend of 10 Natural Oils | 99.8% Natural & 100% Vegan",
+//       pricecut: 3999,
+//       Price: 299,
+//       Review: 105 + " reviews",
+//     },
+//   ];
+//   // it store data in localStorage
+//   localStorage.setItem("skinList", JSON.stringify(data));
+
+//   var products = JSON.parse(localStorage.getItem("skinList")) || [];
+//   // var dvalues=JSON.parse(localStorage.getItem("dilip")) || [];
+
+//   display(products);
+
+//   function display(products) {
+//     products.map(function (items) {
+//       var maindiv = document.createElement("div");
+//       maindiv.setAttribute("id", "maindiv");
+//       var imgdiv = document.createElement("div");
+//       imgdiv.setAttribute("id", "imgdiv");
+//       var offer = document.createElement("div");
+//       offer.setAttribute("id", "offer");
+//       offer.textContent = "10% off";
+//       var img = document.createElement("img");
+//       img.setAttribute("src", items.Img_url);
+//       var div1 = document.createElement("div");
+//       div1.setAttribute("id", "div1");
+//       var rating = document.createElement("div");
+//       rating.setAttribute("id", "rating");
+//       rating.innerHTML = "<span>★★★★☆</span> " + items.Review;
+//       var title = document.createElement("div");
+//       title.textContent = items.Name;
+//       var dash = document.createElement("div");
+//       dash.setAttribute("id", "dash");
+
+//       var pricdiv = document.createElement("div");
+
+//       var price = document.createElement("span");
+//       price.textContent = "₹" + " " + items.Price;
+
+//       var prcut = document.createElement("s");
+//       prcut.textContent = "₹" + " " + items.pricecut;
+//       pricdiv.append(prcut, price);
+
+//       var btn = document.createElement("button");
+//       btn.textContent = "add to cart";
+//       imgdiv.addEventListener("click", function () {
+//         addimg(items);
+//       });
+//       imgdiv.append(offer, img);
+//       div1.append(rating, title, dash, pricdiv);
+//       maindiv.append(imgdiv, div1, btn);
+
+//       // document.getElementById("producttoShow").append(maindiv)
+//     });
+//   }
+
+  var imgs = [];
+  function addimg(items) {
+    imgs.push(items);
+    localStorage.setItem("dvalues", JSON.stringify(imgs));
+    window.location.href = "details.html";
+  }
+  var details = JSON.parse(localStorage.getItem("dvalues"));
+  console.log(details);
+
+  details.map(function (item) {
+    
+    var im = document.createElement("img");
+    im.setAttribute("src", item.Img_url);
+    document.querySelector(".firstimg").append(im);
+    document.querySelector(".avalibdiv>h2").textContent = item.Name;
+    document.getElementById("p").textContent = item.Review;
+    document.getElementById("sp").textContent = "₹" + " " + item.pricecut;
+    document.getElementById("sp1").textContent = "₹" + " " + item.Price;
+  });
+  function fun() {
+    var ip = document.getElementById("ip").value;
+    console.log(ip);
+    console.log(typeof ip);
+    if (ip[0] == "0") {
+      alert("please enter a valid pincode");
+    } else if (ip.length < 6) {
+      alert("please enter a 6 digit pincode");
+    } else if (ip[0] != "0" && ip.length == 6) {
+      document.getElementById("pinbox").textContent = "";
+      document.getElementById("pinbox").textContent =
+        "Shipping Available! There will be a delay in shipping of your order owing to the COVID situation.";
+    }
+  }
+  
+// go to product page
+
+document.getElementById("gotoproductskin").addEventListener("click",function() {
+  window.location.href = "skin.html"
+})
+document.getElementById("gotoproducthair").addEventListener("click",function() {
+  window.location.href = "hair.html"
+})
+// go to login page
+
+document.getElementById("register").addEventListener("click",function() {
+  window.location.href = "signInPage.html"
+})
