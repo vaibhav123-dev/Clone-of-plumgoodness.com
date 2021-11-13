7da5cc42ea004e42c45b5297341b861dfa55bbc5
Avish Kumar <graphicalmanavish@gmail.com>
2021-11-13 19:17:33 +0530

    changes updated

---
+++ js/hairscript.js
@@ -0,0 +1,259 @@
+var data = [
+    { 
+        Img_url:"https://cdn.shopify.com/s/files/1/0390/2985/products/1avocado_150x.jpg?v=1630919840",
+        Name:"Avocado Daily Haircare Kit | For Frizzy Hair | Silicone & Sulphate Free | FREE Neem Comb",
+        Price: 310,
+        Review:1167 + " reviews",
+    },
+    { 
+        Img_url: "https://cdn.shopify.com/s/files/1/0390/2985/products/1_603073d5-bfde-4502-824e-eb52c1600c4b_150x.jpg?v=1630921441",
+        Name:"Ginseng Hair Fall Rescue Duo | Limited Edition | Prevents Breakage",
+        Price: 423,
+        Review:157 + " reviews",
+    },
+    { 
+        Img_url:"https://cdn.shopify.com/s/files/1/0390/2985/products/06_OM_New_1_1f494331-70c5-4d1b-9857-9452e83f831a_150x.jpg?v=1630921781",
+        Name:"Olive & Macadamia Get Nourished Trio | Dull, Dry, Damaged Hair | Sulphate Free | 100% Vegan | With Free Hair Oil",
+        Price: 427,
+        Review:99 + " reviews",
+    },
+    { 
+        Img_url:"https://cdn.shopify.com/s/files/1/0390/2985/products/1_3646ee52-ec42-431b-a6c7-96e9556d2566_150x.jpg?v=1630919818",
+        Name:"Avocado Daily & Weekly Anti-Frizz Hair Kit | For Smooth & Shiny Hair | 100% Vegan | FREE Neem Comb",
+        Price: 351,
+        Review:1285 + " reviews",
+    },
+    { 
+        Img_url:"https://cdn.shopify.com/s/files/1/0390/2985/products/1_AvocadoFrizzFreeDailyRegime_01b_150x.jpg?v=1630919859",
+        Name:"Avocado Frizz Free Daily Hair Regime | For Frizz-Free Hair  | Goodness of Shea Butter, Argan Oil and Avocados | Silicone & Sulphate Free | Neem Comb Free",
+        Price: 265,
+        Review:227 + " reviews",
+    },
+    { 
+        Img_url:"https://cdn.shopify.com/s/files/1/0390/2985/products/1_a921642f-0a81-4ef4-848e-c313ba91c2c3_150x.jpg?v=1630921339",
+        Name:"Olive & Macadamia Hair Care Trio with Satin Headband",
+        Price: 423,
+        Review:789 + " reviews",
+    },
+    { 
+        Img_url:"https://cdn.shopify.com/s/files/1/0390/2985/products/WBG_4_150x.jpg?v=1630921828",
+        Name:"Olive & Macadamia Hair Care Trio with Satin Headband",
+        Price: 499,
+        Review:207 + " reviews",
+    },
+    { 
+        Img_url:"https://cdn.shopify.com/s/files/1/0390/2985/products/Shampoo_Mask_1_150x.jpg?v=1630920936",
+        Name:"Daily + Weekly Hair Care Duo  | Dry, Dull Damaged Hair | Sulphate Free | 100% Vegan",
+        Price: 441,
+        Review:881 + " reviews",
+    },
+    { 
+        Img_url:"https://cdn.shopify.com/s/files/1/0390/2985/products/2-A_150x.jpg?v=1630921412",
+        Name:"Dull and Dry Hair Intense Repair Treatment | For Smooth & Shiny Hair | Suitable for Chemically Treated Hair",
+        Price: 449,
+        Review:97 + " reviews",
+    },
+    { 
+        Img_url:"https://cdn.shopify.com/s/files/1/0390/2985/products/1_c4d5b9d3-183c-4098-8efc-bf00f791ca29_150x.jpg?v=1635398822",
+        Name:"Ginseng Anti-Hair Fall Weekly Treatment | Limited Edition | Prevents Breakage & Strengthensn",
+        Price: 305,
+        Review:225 + " reviews",
+    },
+    { 
+        Img_url:"https://cdn.shopify.com/s/files/1/0390/2985/products/1a.WBG_150x.jpg?v=1632452616",
+        Name:"Avocado Nourish-Up Hair Mask | For Frizz-Free Hair | Contains Avocado Oil, Argan Oil & Shea Butter",
+        Price: 269,
+        Review:353 + " reviews",
+    },
+    { 
+        Img_url:"https://cdn.shopify.com/s/files/1/0390/2985/products/1a.WBG_Front_1_150x.jpg?v=1630919938",
+        Name:"Avocado Smoothin' It Conditioner | For Frizz-Free & Smooth Hair | Contains Shea Butter & Almond Oil | Silicone-Free",
+        Price: 549,
+        Review:568 + " reviews",
+    },
+    { 
+        Img_url:"https://cdn.shopify.com/s/files/1/0390/2985/products/GinsengRootNourishHairOil_1_150x.jpg?v=1630921463",
+        Name:"Ginseng Root Nourish Hair Oil | Pre-Shampoo Hair Oil With Amla & Almond Oil | Promotes Hair Growth & Controls Hair Fall",
+        Price: 298,
+        Review:451 + " reviews",
+    },
+    { 
+        Img_url:"https://cdn.shopify.com/s/files/1/0390/2985/products/WBG_1_45649619-9b05-45f3-9ec7-ea17fea4f32b_150x.jpg?v=1630921893",
+        Name:"Olive & Macadamia Healthy Hydration Shampoo | Dull, Dry, Damaged Hair | Safe for Chemically Treated Hair",
+        Price: 788,
+        Review:235 + " reviews",
+    },
+    { 
+        Img_url:"https://cdn.shopify.com/s/files/1/0390/2985/products/GinsengFallRescueScalpSerum_1_150x.jpg?v=1630921360",
+        Name:"Ginseng Fall Rescue Scalp Serum | Strengthens roots and Promotes hair growth | Silicone-free",
+        Price: 333,
+        Review:121 + " reviews",
+    },
+    { 
+        Img_url:"https://cdn.shopify.com/s/files/1/0390/2985/products/WBG_1_150x.jpg?v=1630921964",
+        Name:"Olive & Macadamia Mega Moisturizing Hair Mask | Hair Spa | Safe for Chemically Treated Hair |100% Vegan",
+        Price: 635,
+        Review:254 + " reviews",
+    },
+    { 
+        Img_url:"https://cdn.shopify.com/s/files/1/0390/2985/products/WBG_2_150x.jpg?v=1630922002",
+        Name:"Olive & Macadamia Nutri-Shield Pre-Shampoo Hair Oil | 8 Natural Oil for Damaged Hair | Argan, Jojoba Oils | 100% Vegan",
+        Price: 689,
+        Review:254 + " reviews",
+    },
+    { 
+        Img_url:"https://cdn.shopify.com/s/files/1/0390/2985/products/1WBG_015e804d-92af-411a-830e-9760badad6c7_150x.jpg?v=1630922043",
+        Name:"Olive & Macadamia Rich Nourish Conditioner | For Smooth & Shiny Hair | Dull, Dry, Damaged Hair | 100% Vegan",
+        Price: 310,
+        Review:302 + " reviews",
+    },
+    { 
+        Img_url:"https://cdn.shopify.com/s/files/1/0390/2985/products/Daily_weeklycareduo_1_2eda4de7-d704-464f-8c21-4a769967cad2_150x.jpg?v=1630922110",
+        Name:"Olive & Macadamia Shampoo + Mask for Dry, Damaged Hair",
+        Price: 420,
+        Review:401 + " reviews",
+    },
+    { 
+        Img_url:"https://cdn.shopify.com/s/files/1/0390/2985/products/3_AvocadoAntiFrizzTreatment_01a_150x.jpg?v=1634203361",
+        Name:"Avocado Frizz Control Treatment | Prevents Frizz | Smooth & Shiny Hair | 100% Vegan",
+        Price: 423,
+        Review:213 + " reviews",
+    },
+
+
+]
+
+// it store data in localStorage
+    localStorage.setItem("hairList", JSON.stringify(data))
+
+var products=JSON.parse(localStorage.getItem("hairList")) || [];
+display(products)
+// sortbyprice
+function sortbyprice() {
+  
+    var val = document.getElementById("selectSort").value;
+    if (val == "Low") {   
+        products = products.sort(function (a, b) {   
+            return a.Price - b.Price;
+        });
+    }
+    if(val=="High") { 
+        products = products.sort(function (a, b) {
+            return b.Price - a.Price;
+        })
+    }
+
+    console.log(products)
+    display(products);
+}
+
+
+function skin() {
+  
+    var x = document.querySelector(".sk").value;
+    console.log(x);
+  products= products.filter(function(item){
+        return (item.type=="skin")
+    })
+console.log(products);
+            
+            display(products);   
+    
+}
+
+
+function hair() {
+     
+    
+    var h = document.querySelector(".hair").value;
+    console.log(h);
+    products = products.filter(function (item) {
+        return (item.type == "hair");
+    })
+    console.log(products);
+    display(products);
+}
+
+function display(products){
+    document.getElementById("producttoShow").innerHTML="";
+    products.map(function(items){
+        var maindiv = document.createElement("div")
+        maindiv.setAttribute("id", "maindiv")
+        var imgdiv= document.createElement("div")
+        imgdiv.setAttribute("id", "imgdiv")
+        var offer = document.createElement("div")
+        offer.setAttribute("id", "offer")
+        offer.textContent="10% off"
+        var img = document.createElement("img")
+        img.setAttribute("src", items.Img_url)
+        var div1= document.createElement("div")
+        div1.setAttribute("id", "div1")
+        var rating = document.createElement("div")
+        rating.setAttribute("id", "rating")
+        rating.innerHTML= "<span>★★★★☆</span> " + items.Review;
+        var title= document.createElement("div")
+        title.textContent = items.Name;
+        var dash= document.createElement("div")
+        dash.setAttribute("id", "dash")
+        var price= document.createElement("div")
+        price.textContent = "₹" +" " +items.Price;
+        var btn= document.createElement("button")
+        btn.textContent="add to cart"
+        btn.addEventListener("click",function(){
+            storeItem(items);})
+        imgdiv.append(offer, img)
+    
+        div1.append(rating,title,dash,price)
+        maindiv.append(imgdiv,div1, btn)
+        document.getElementById("producttoShow").append(maindiv)
+        imgdiv.addEventListener("click", function () {
+            addimg(items);
+        })
+    })
+}
+// it will store add to cart item in local storage 
+var cart = [];
+function storeItem(items) {
+  var flag = false;
+  cart.map(function (value) {
+    if (items == value) {
+      flag = true;
+    }
+  });
+  if (!flag) {
+    cart.push(items);
+  } 
+
+  localStorage.setItem("Usercart", JSON.stringify(cart));
+}
+var accordion = document.getElementsByClassName("contentBx");
+
+for (var i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+  });
+}
+
+// sideMenu
+// sideMenu
+function openMenu() {
+    document.getElementById("mySideMenu").style.width = "350px";
+    
+  }
+  
+  function closeMenu() {
+    document.getElementById("mySideMenu").style.width = "0";
+  }
+//  gotopay
+document.querySelector(".gotopay").addEventListener("click",function() {
+window.location.href = "checkOut.html"
+})
+// gotoskinpage
+document.getElementById("gotoproductskin").addEventListener("click",function() {
+    window.location.href = "skin.html"
+  })
+  // go to login page
+
+document.getElementById("register").addEventListener("click",function() {
+    window.location.href = "signInPage.html"
+  })
