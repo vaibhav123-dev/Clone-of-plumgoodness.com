e9b8ae3006a4cc1aa4167dc24d2e7f45d3bd773f
Avish Kumar <graphicalmanavish@gmail.com>
2021-11-13 17:43:48 +0530

    Initiate the construct week project

---
+++ js/home_js.js
@@ -0,0 +1,17 @@
+
+
+// go to product page
+
+document.getElementById("gotoproductskin").addEventListener("click",function() {
+  window.location.href = "skin.html"
+})
+document.getElementById("gotoproducthair").addEventListener("click",function() {
+  window.location.href = "hair.html"
+})
+
+// go to login page
+
+document.getElementById("register").addEventListener("click",function() {
+  window.location.href = "signInPage.html"
+})
+ 
